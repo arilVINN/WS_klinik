3a559316c03f0a85f8a0a49a1988a2fb344837be
arilVINN <arieltheardhian@gmail.com>
2026-07-23 21:14:20 +0700

    dokumentasi api swagger

---
+++ jadwal_service/jadwal/src/main/java/com/klinik/jadwal/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.klinik.jadwal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Jadwal Service API")
+                        .version("v1")
+                        .description("Dokumentasi API untuk layanan jadwal klinik")
+                        .contact(new Contact()
+                                .name("Tim Klinik")
+                                .email("support@klinik.local")
+                        )
+                );
+    }
+}
--- jadwal_service/jadwal/src/main/java/com/klinik/jadwal/controller/JadwalController.java
+++ jadwal_service/jadwal/src/main/java/com/klinik/jadwal/controller/JadwalController.java
@@ -2,6 +2,8 @@ package com.klinik.jadwal.controller;
 
 import com.klinik.jadwal.model.JadwalModel;
 import com.klinik.jadwal.service.JadwalService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,16 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/jadwal")
+@Tag(name = "Jadwal", description = "API untuk mengelola jadwal layanan klinik")
 public class JadwalController {
     @Autowired
     private JadwalService js;
 
     @PostMapping
+    @Operation(summary = "Buat jadwal baru", description = "Menambahkan jadwal pemeriksaan pasien ke sistem")
     public boolean createJadwal(@RequestBody JadwalModel jadwal) {
         return js.createJadwal(jadwal);
     }
 
     @GetMapping
+    @Operation(summary = "Dapatkan semua jadwal", description = "Mengambil daftar semua jadwal pemeriksaan")
     public List<JadwalModel> getAllJadwal() {
         return js.getAllJadwal();
     }
